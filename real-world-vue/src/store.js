42f07e8ac7904aae750caf3dfbc81891b66371c7
Mrs Maveric <missbodre@gmail.com>
2019-02-25 23:30:06 +1100

    Adding Vuex structures.

--- real-world-vue/src/store.js
+++ real-world-vue/src/store.js
@@ -4,11 +4,29 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: { count: 0 },
-  mutations: {
-    INCREMENT_COUNT(state) {
-      state.count += 1
-    }
+  state: {
+    user: { id: 'abc123', name: 'Gyuri Kim' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    events: [
+      { id: 1, title: '...', organizer: '...' },
+      { id: 2, title: '...', organizer: '...' },
+      { id: 3, title: '...', organizer: '...' },
+      { id: 4, title: '...', organizer: '...' }
+    ]
   },
-  actions: {}
+  mutations: {},
+  actions: {},
+  getters: {
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  }
 })
