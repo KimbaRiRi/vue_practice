8404aa452a5d53ff47ffbe3c4702f39127ef6a01
Mrs Maveric <missbodre@gmail.com>
2019-02-26 12:38:58 +1100

    testing mutation and action functions in vuex store

--- real-world-vue/src/store.js
+++ real-world-vue/src/store.js
@@ -5,6 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 0,
     user: { id: 'abc123', name: 'Gyuri Kim' },
     categories: [
       'sustainability',
@@ -22,8 +23,18 @@ export default new Vuex.Store({
       { id: 4, title: '...', organizer: '...' }
     ]
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    INCREMENT_COUNT(state, value) {
+      state.count += value
+    }
+  },
+  actions: {
+    updateCount({ state, commit }, incrementBy) {
+      if (state.user) {
+        commit('INCREMENT_COUNT', incrementBy)
+      }
+    }
+  },
   getters: {
     getEventById: state => id => {
       return state.events.find(event => event.id === id)
--- real-world-vue/src/views/EventCreate.vue
+++ real-world-vue/src/views/EventCreate.vue
@@ -3,15 +3,32 @@
     <h1>Create an Event, {{ user.name }}</h1>
     <p>This event was created by {{ user.name }}</p>
     <p>{{ getEventById(2) }}</p>
+    <button @click="incrementCount">Increment</button>
+    <input type="number" v-model.number="incrementBy" />
   </div>
 </template>
 
 <script>
 import { mapState, mapGetters } from 'vuex'
 export default {
+  data() {
+    return {
+      incrementBy: 1
+    }
+  },
   computed: {
     ...mapGetters(['getEventById']),
     ...mapState(['user', 'categories'])
+  },
+  methods: {
+    //mutation testing
+    // incrementCount() {
+    //   this.$store.commit('INCREMENT_COUNT', this.incrementBy)
+    // }
+    //action testing
+    incrementCount() {
+      this.$store.dispatch('updateCount', this.incrementBy)
+    }
   }
 }
 </script>
